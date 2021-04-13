76cb1a8236f2ec99e8a9076a2826ab75c487176a
ShadolGyje <82037760+ShadolGyje@users.noreply.github.com>
2021-04-13 20:37:00 +0700

    Добавлен маштаб спрайтов, а так же спрайт мяча

--- ping_pong.py
+++ ping_pong.py
@@ -2,9 +2,9 @@ from pygame import *
 from random import randint 
 from time import time as timer
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed):
+    def __init__(self, player_image, player_x, player_y, player_width, player_height, player_speed):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (70,65))
+        self.image = transform.scale(image.load(player_image), (player_width ,player_height))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -33,9 +33,9 @@ FPS = 60
 
 window = display.set_mode((700, 500))
 display.set_caption("Понг")
-player1 = Player('racket.jpeg',320, 400, 15)
-player2 = Player('racket.jpeg',320, 400, 15)
-
+player1 = Player('racket.jpeg',50, 200, 65, 100, 10)
+player2 = Player('racket.jpeg',590, 200, 65, 100, 10)
+ball = GameSprite('ball.jpeg',320, 200, 70, 70, 8 )
 
 game = True
 finish = False
@@ -53,11 +53,11 @@ while game:
                 last_time = timer()
 
     if finish != True:
-        window.fill((255,255,255))
+        window.fill((255,250,255))
         player1.reset()
         player1.update_l()
         player2.reset()
         player2.update_r()
         clock.tick(FPS)
-
+        ball.reset()
     display.update()
