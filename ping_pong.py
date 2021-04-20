603af6db9d9ed29118cff41f0f7f1b218b64a778
ShadolGyje <82037760+ShadolGyje@users.noreply.github.com>
2021-04-20 19:57:31 +0700

    Add files via upload
    Мяч может отскакивать от ракеток и нижний и верхней границы экрана, а так же добавлено условие проигрыша

--- ping_pong.py
+++ ping_pong.py
@@ -16,13 +16,13 @@ class Player(GameSprite):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_UP] and self.rect.y > 0:
             self.rect.y -= self.speed
-        if keys_pressed[K_DOWN] and self.rect.y < 450:
+        if keys_pressed[K_DOWN] and self.rect.y < 400:
             self.rect.y += self.speed
     def update_r(self):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_w] and self.rect.y > 0:
             self.rect.y -= self.speed
-        if keys_pressed[K_s] and self.rect.y < 450:
+        if keys_pressed[K_s] and self.rect.y < 400:
             self.rect.y += self.speed
 
 lost = 0
@@ -33,9 +33,17 @@ FPS = 60
 
 window = display.set_mode((700, 500))
 display.set_caption("Понг")
-player1 = Player('racket.jpeg',50, 200, 65, 100, 10)
-player2 = Player('racket.jpeg',590, 200, 65, 100, 10)
-ball = GameSprite('ball.jpeg',320, 200, 70, 70, 8 )
+player1 = Player('racket.png',50, 200, 65, 100, 10)
+player2 = Player('racket.png',590, 200, 65, 100, 10)
+ball = GameSprite('ball.png',320, 200, 70, 70, 8 )
+
+speed_x = 3
+speed_y = 3
+
+font.init()
+font1 = font.SysFont('Arial', 30)
+dead1 = font1.render('1 Player dead', True, (205, 245, 30))
+dead2 = font1.render('2 Player dead', True, (105, 45, 30))
 
 game = True
 finish = False
@@ -51,7 +59,7 @@ while game:
             if num_fire>=5 and rel_time == False:
                 rel_time = True
                 last_time = timer()
-
+        
     if finish != True:
         window.fill((255,250,255))
         player1.reset()
@@ -60,4 +68,21 @@ while game:
         player2.update_r()
         clock.tick(FPS)
         ball.reset()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y < 0:
+            speed_y = speed_y * -1
+        if ball.rect.y > 430:
+            speed_y = speed_y * -1
+        
+        if sprite.collide_rect(player1 , ball) or sprite.collide_rect(player2 , ball): 
+            speed_x *= -1
+        
+        if ball.rect.x < 0:
+            window.blit(dead1,(240,300))
+            finish = True
+        if ball.rect.x > 750:
+            window.blit(dead2,(240,300))
+            finish = True
+        
     display.update()
